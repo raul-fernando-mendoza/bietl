5858a20c36f6b2529c41a17ed120e391121fcd10
Raul Mendoza <raul.mendoza@softtek.com>
2020-06-14 13:26:59 -0500

    first version with user validation in page /login uses create-drop to recreate the database

--- spring-security-jpa/sqls/user_inserts.sql
+++ spring-security-jpa/sqls/user_inserts.sql
@@ -1,2 +1,11 @@
+drop user eApp@localhost;
+CREATE USER eApp@localhost IDENTIFIED BY 'odroid';
+GRANT ALL PRIVILEGES ON entities.database_name to eApp@localhost;
+GRANT ALL PRIVILEGES ON *.* TO 'root'@'192.168.15.%' IDENTIFIED BY 'odroid' WITH GRANT OPTION;
+
+FLUSH PRIVILEGES;
+
 INSERT INTO `user` (`id`, `active`, `password`, `roles`, `user_name`) VALUES (0, b'1', 'odroid', 'ROLE_USER,ROLE_ADMIN', 'admin');
 INSERT INTO `user` (`id`, `active`, `password`, `roles`, `user_name`) VALUES (1, b'1', 'odroid', 'ROLE_USER', 'user');
+
+
--- spring-security-jpa/src/main/java/io/javabrains/springsecurityjpa/SpringSecurityJpaApplication.java
+++ spring-security-jpa/src/main/java/io/javabrains/springsecurityjpa/SpringSecurityJpaApplication.java
@@ -2,11 +2,13 @@ package io.javabrains.springsecurityjpa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
 
 @SpringBootApplication
 @EnableJpaRepositories(basePackageClasses = UserRepository.class)
-public class SpringSecurityJpaApplication {
+public class SpringSecurityJpaApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringSecurityJpaApplication.class, args);
